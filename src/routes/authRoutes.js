00fff2f849c5cad359368cc9df2d5f4ef841a13f
Biplab Tarafder <biplab.tarafder2001@gmail.com>
2024-02-26 16:10:02 +0530

    modified file name

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { signupUser, loginUser } = require("../controllers/userCont");
+const { signupUser, loginUser } = require("../controllers/userCtrl");
 
 const router = express.Router();
 
