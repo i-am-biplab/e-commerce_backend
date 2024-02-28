b5f97ab57d742eb9eb292bc09e92936abf04c1e5
Biplab Tarafder <biplab.tarafder2001@gmail.com>
2024-02-28 19:43:46 +0530

    add route of product

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("./models/index");
 
 const authRoutes = require("./routes/authRoutes");
-// const productRoutes = require("./routes/productRoutes");
+const productRoutes = require("./routes/productRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
 const port = process.env.PORT || 7000;
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
-// app.use("/product", productRoutes);
+app.use("/product", productRoutes);
 app.use("/admin", adminRoutes);
 
 app.get("/", (req, res) => {
